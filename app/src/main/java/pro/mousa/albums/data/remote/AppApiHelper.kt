f43a4494c39dc732dcd587f08022d31a4b5f34a9
Mousa Moradi <mousamk@gmail.com>
2019-01-04 23:38:14 +0330

    Saved data after downloading.

--- app/src/main/java/pro/mousa/albums/data/remote/AppApiHelper.kt
+++ app/src/main/java/pro/mousa/albums/data/remote/AppApiHelper.kt
@@ -2,15 +2,17 @@ package pro.mousa.albums.data.remote
 
 import io.reactivex.Single
 import pro.mousa.albums.Config
+import pro.mousa.albums.data.local.DbHelper
 import pro.mousa.albums.data.model.Album
 import pro.mousa.albums.data.model.Photo
 import pro.mousa.albums.data.model.User
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Inject
 
 
-class AppApiHelper : ApiHelper
+class AppApiHelper @Inject constructor(private val dbHelper: DbHelper) : ApiHelper
 {
     private val apiService: ApiService
 
@@ -26,16 +28,25 @@ class AppApiHelper : ApiHelper
 
     override fun getAlbums(): Single<List<Album>>
     {
-        return apiService.getAlbums()
+        return apiService.getAlbums().map { albums ->
+            dbHelper.saveAlbums(albums)
+            albums
+        }
     }
 
     override fun getPhotos(): Single<List<Photo>>
     {
-        return apiService.getPhotos()
+        return apiService.getPhotos().map { photos ->
+            dbHelper.savePhotos(photos)
+            photos
+        }
     }
 
     override fun getUsers(): Single<List<User>>
     {
-        return apiService.getUsers()
+        return apiService.getUsers().map { users ->
+            dbHelper.saveUsers(users)
+            users
+        }
     }
 }
