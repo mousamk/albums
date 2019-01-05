77cd95a7da8476cb1278d3d50d43843ee25baf73
Mousa Moradi <mousamk@gmail.com>
2019-01-05 04:02:54 +0330

    Created base classes for MVVM arch.

--- app/src/main/java/pro/mousa/albums/ui/base/BaseActivity.kt
+++ app/src/main/java/pro/mousa/albums/ui/base/BaseActivity.kt
@@ -7,6 +7,8 @@ import android.app.Activity
  * This is the base class for activities in this application. It exists to hold the shared
  * properties and also provide a common class for dependencies to hook on.
  */
-abstract class BaseActivity : Activity()
+abstract class BaseActivity<out V : BaseViewModel<out BaseNavigator>> : Activity()
 {
+    abstract val layoutId: Int
+    abstract val viewModel: V
 }
