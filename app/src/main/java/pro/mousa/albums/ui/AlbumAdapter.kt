b90cc2499bac16b3ae6b2263294c4bc430ae3f28
Mousa Moradi <mousamk@gmail.com>
2019-01-06 01:07:55 +0330

    Showing multiple images of albums.

--- app/src/main/java/pro/mousa/albums/ui/AlbumAdapter.kt
+++ app/src/main/java/pro/mousa/albums/ui/AlbumAdapter.kt
@@ -33,18 +33,48 @@ class AlbumAdapter(context: Context,
         val album = albums[position]
         holder.albumNameView.text = album.title
         holder.userNameView.text = album.user?.name ?: "User ID ${album.userId}"
-        holder.previewImageView
-        Picasso.get()
-            .load(album.photos?.get(0)?.thumbnailUrl)
-            .error(R.drawable.placeholder).placeholder(R.drawable.placeholder)
-            .fit().centerCrop()
-            .into(holder.previewImageView)
+        setPhotos(holder, album)
+    }
+
+    private fun setPhotos(holder: AlbumViewHolder, album: Album)
+    {
+        album.photos?.let { photos ->
+            if (photos.isNotEmpty()) {
+                Picasso.get()
+                    .load(photos[0].thumbnailUrl)
+                    .error(R.drawable.placeholder).placeholder(R.drawable.placeholder)
+                    .fit().centerCrop()
+                    .into(holder.previewImageView1)
+            }
+            if (photos.size > 1) {
+                holder.previewImageView2.visibility = View.VISIBLE
+                Picasso.get()
+                    .load(photos[1].thumbnailUrl)
+                    .error(R.drawable.placeholder).placeholder(R.drawable.placeholder)
+                    .fit().centerCrop()
+                    .into(holder.previewImageView2)
+            }
+            else
+                holder.previewImageView2.visibility = View.GONE
+            if (photos.size > 2) {
+                holder.previewImageView3.visibility = View.VISIBLE
+                Picasso.get()
+                    .load(photos[2].thumbnailUrl)
+                    .error(R.drawable.placeholder).placeholder(R.drawable.placeholder)
+                    .fit().centerCrop()
+                    .into(holder.previewImageView3)
+            }
+            else
+                holder.previewImageView3.visibility = View.GONE
+        }
     }
 
 
     class AlbumViewHolder(root: View) : RecyclerView.ViewHolder(root)
     {
-        internal val previewImageView: ImageView = itemView.findViewById(R.id.previewImageView)
+        internal val previewImageView1: ImageView = itemView.findViewById(R.id.previewImageView1)
+        internal val previewImageView2: ImageView = itemView.findViewById(R.id.previewImageView2)
+        internal val previewImageView3: ImageView = itemView.findViewById(R.id.previewImageView3)
         internal val albumNameView: TextView = itemView.findViewById(R.id.albumNameView)
         internal val userNameView: TextView = itemView.findViewById(R.id.userNameView)
     }
