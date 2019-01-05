73cc0aa57d2c2f1f224703c56d0e8e1496e8ee08
Mousa Moradi <mousamk@gmail.com>
2019-01-05 07:07:12 +0330

    Using AppCompatActivity.

--- app/src/main/java/pro/mousa/albums/ui/base/BaseActivity.kt
+++ app/src/main/java/pro/mousa/albums/ui/base/BaseActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import dagger.android.AndroidInjection
 
 
@@ -11,7 +12,7 @@ import dagger.android.AndroidInjection
  * This is the base class for activities in this application. It exists to hold the shared
  * properties and also provide a common class for dependencies to hook on.
  */
-abstract class BaseActivity<out B : ViewDataBinding, out V : BaseViewModel<out BaseNavigator>> : Activity()
+abstract class BaseActivity<out B : ViewDataBinding, out V : BaseViewModel<out BaseNavigator>> : AppCompatActivity()
 {
     abstract val layoutId: Int
     private var viewModelBack: V? = null
