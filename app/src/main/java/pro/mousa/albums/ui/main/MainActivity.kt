f266b16fd950075bc8673d219f0fb6dfeabfd2fe
Mousa Moradi <mousamk@gmail.com>
2019-01-05 04:04:50 +0330

    MainActivity exports its opening intent.

--- app/src/main/java/pro/mousa/albums/ui/main/MainActivity.kt
+++ app/src/main/java/pro/mousa/albums/ui/main/MainActivity.kt
@@ -1,5 +1,7 @@
 package pro.mousa.albums.ui.main
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import pro.mousa.albums.R
 import pro.mousa.albums.ui.base.BaseActivity
@@ -11,5 +13,11 @@ class MainActivity : BaseActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    companion object
+    {
+        fun newIntent(context: Context) = Intent(context, MainActivity::class.java)
     }
 }
