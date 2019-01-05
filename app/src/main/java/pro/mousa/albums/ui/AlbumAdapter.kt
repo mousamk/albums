75dc388c9f37139e7e7a217c3ea24a6b972a0c1e
Mousa Moradi <mousamk@gmail.com>
2019-01-05 23:34:51 +0330

    Albums list is now being shown.

---
+++ app/src/main/java/pro/mousa/albums/ui/AlbumAdapter.kt
@@ -0,0 +1,51 @@
+package pro.mousa.albums.ui
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
+import pro.mousa.albums.R
+import pro.mousa.albums.data.model.Album
+
+
+class AlbumAdapter(context: Context,
+                   private val albums: List<Album>,
+                   private val albumClickHandler: (Album)->Unit)
+    : RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder>()
+{
+    private val layoutInflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): AlbumViewHolder
+    {
+        val view = layoutInflater.inflate(R.layout.item_album, parent, false)
+        return AlbumViewHolder(view)
+    }
+
+    override fun getItemCount() = albums.size
+
+    override fun onBindViewHolder(holder: AlbumViewHolder, position: Int)
+    {
+        val album = albums[position]
+        holder.albumNameView.text = album.title
+        holder.userNameView.text = album.userId.toString()  //TODO
+        holder.previewImageView
+        Picasso.get()
+            .load("image address!")     //TODO
+            .error(R.drawable.placeholder).placeholder(R.drawable.placeholder)
+            .fit().centerCrop()
+            .into(holder.previewImageView)
+    }
+
+
+    class AlbumViewHolder(root: View) : RecyclerView.ViewHolder(root)
+    {
+        internal val previewImageView: ImageView = itemView.findViewById(R.id.previewImageView)
+        internal val albumNameView: TextView = itemView.findViewById(R.id.albumNameView)
+        internal val userNameView: TextView = itemView.findViewById(R.id.userNameView)
+    }
+}
--- app/src/main/java/pro/mousa/albums/ui/AlbumsFragment.kt
+++ app/src/main/java/pro/mousa/albums/ui/AlbumsFragment.kt
@@ -1,14 +1,89 @@
 package pro.mousa.albums.ui
 
-import android.widget.Toast
+import android.content.Context
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import pro.mousa.albums.R
 import pro.mousa.albums.data.model.Album
+
+import kotlinx.android.synthetic.main.fragment_albums.*
 
 
 class AlbumsFragment : BaseFragment()
 {
-    fun showAlbum(album: Album)
+    private var albumAdapter: AlbumAdapter? = null
+    private lateinit var albums: List<Album>
+    private var interactionListener: InteractionListener? = null
+
+
+    override fun onAttach(context: Context?)
     {
-        //TODO: ...
-        Toast.makeText(context, "Show album [${album.title}]!", Toast.LENGTH_SHORT).show()
+        super.onAttach(context)
+        val candidates = arrayOf(parentFragment, targetFragment, getContext())
+        for (parent in candidates)
+            if (parent is InteractionListener) {
+                interactionListener = parent
+                return
+            }
+    }
+
+    override fun onDetach()
+    {
+        super.onDetach()
+        interactionListener = null
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedState: Bundle?): View?
+    {
+        return inflater.inflate(R.layout.fragment_albums, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedState: Bundle?)
+    {
+        super.onViewCreated(view, savedState)
+        loadData()
+    }
+
+    private fun loadData()
+    {
+        val disposable = dataManager.getAlbums().subscribe { albums: List<Album> ->
+            this@AlbumsFragment.albums = albums
+            if (albums.isEmpty())
+                showEmptyNote()
+            else
+                showAlbumsList()
+        }
+        disposables.add(disposable)
+    }
+
+    private fun showEmptyNote()
+    {
+        emptyNoteView?.visibility = View.VISIBLE
+        albumsRecyclerView?.visibility = View.INVISIBLE
+    }
+
+    private fun showAlbumsList()
+    {
+        albumAdapter = AlbumAdapter(context!!, albums) { interactionListener?.onAlbumClick(it) }
+        albumsRecyclerView?.layoutManager = LinearLayoutManager(context)
+        albumsRecyclerView.adapter = albumAdapter
+    }
+
+
+    interface InteractionListener
+    {
+        fun onAlbumClick(album: Album)
+    }
+
+
+    companion object
+    {
+        fun newInstance(): AlbumsFragment
+        {
+            return AlbumsFragment()
+        }
     }
 }
